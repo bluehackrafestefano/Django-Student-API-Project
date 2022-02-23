2d890e6f58f56c8bd5dcf7168e39d5b34955ae12
rafe <rafe@clarusway.com>
2022-02-23 13:33:06 +0100

    added serializermodelfield

--- student_api/models.py
+++ student_api/models.py
@@ -11,6 +11,7 @@ class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     number = models.IntegerField(blank=True, null=True)
+    register_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.last_name} {self.first_name}"
--- student_api/serializers.py
+++ student_api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Student, Path
+from django.utils.timezone import now
 
 class PathSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,6 +8,14 @@ class PathSerializer(serializers.ModelSerializer):
         fields = ["id", "path_name"]
 
 class StudentSerializer(serializers.ModelSerializer):
+    
+    days_since_joined = serializers.SerializerMethodField(label="a")
+    minutes_since_joined = days_since_joined
+    
     class Meta:
         model = Student
+        # fields = ["first_name", "number", "days_since_joined", "minutes_since_joined"]
         fields = "__all__"
+        
+    def get_days_since_joined(self, obj):
+        return ((now() - obj.register_date).seconds) // 60
